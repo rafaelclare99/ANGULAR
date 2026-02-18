bbbc927f798ada7cad51102fabfa48a9d9e6bd09
Rafael Clare <rafael.clare@hotmail.com>
2026-02-17 21:23:24 -0300

    criando um componente pela cli

--- angular1/src/app/app.ts
+++ angular1/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HelloWorldComponent } from './helloworld/helloworld.component'
+import { Minhapagina } from './minhapagina/minhapagina';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent],
+  imports: [HelloWorldComponent, Minhapagina],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
